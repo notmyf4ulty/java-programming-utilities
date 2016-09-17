29006ac9c9d93c38e3036d84f0fb313571860981
notmyf4ulty <notmyf4ulty@github.com>
2016-09-17 17:35:06 +0200

    Updated MainTest class.

--- templates/junit/MainTest.java
+++ templates/junit/MainTest.java
@@ -1,15 +1,18 @@
+package com.catnbear;
+
+// NOTE: Import class to be tested.
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class MainTest {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(TestJunit.class);
-		
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
-		
-      System.out.println("Tests finished with " + (result.wasSuccessful() ? "success" : "failure") + ".");
-   }
-}  	
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(YOUR_CLASS_TO_BE_TESTED.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println("Tests finished with " + (result.wasSuccessful() ? "success" : "failure") + ".");
+    }
+} 
